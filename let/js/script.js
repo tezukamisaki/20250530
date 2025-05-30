f4516d06b07aa1c10fc23aa116e95c11571d45ed
tezukamisaki <167516877+tezukamisaki@users.noreply.github.com>
2025-05-30 18:38:53 +0900

    Add files via upload

---
+++ let/js/script.js
@@ -0,0 +1,19 @@
+//変数numを宣言
+let num;
+//変数numに3を投入
+num = 3;
+
+console.log(num); //3
+
+//
+let num2 = 5;
+console.log(num2); //5
+
+num = 5;
+
+console.log(num);
+
+//
+
+let str = "hello, world!";
+console.log(str);
---
+++ operator/js/script.js
@@ -0,0 +1,79 @@
+let add = 10 + 5 ;
+
+console.log(add);
+
+let str = "java" + "script";
+console.log(str);
+
+let result1=3 + 3;
+let r0sult2 = "3" + "3";
+
+console.log(result1);
+console.log(result2);
+
+let sub = 10 - 5 ;
+console.log(sub);
+
+let mul = 10 * 5 ;
+console.log(mul);
+
+let div = 10 /5 ;
+console.log(div);
+
+let incr = 10;
+incr++;
+console.log(incr);
+
+let decr = 10;
+decr--;
+console.log(incr);
+
+
+//代入演算子
+
+// 代入して代入
+
+let add2 = 1;
+add2 += 2;
+console.log();
+
+// して代入
+
+let str2 = "java";
+str2 += "script"
+console.log(str2);
+
+
+// 減算して代入
+let sub2 = 10;
+sub2 -= 5; // 10 - 5
+console.log(sub2); // 5
+
+// 乗算して代入
+let mul2 = 10;
+mul2 *= 5; // 10 x 5
+console.log(mul2); // 50
+
+// 除算して代入
+let div2 = 10;
+div2 /= 5; // 10 ÷ 5
+console.log(div2); // 2
+
+
+
+// 比較演算子
+
+let isEqual = (10 == 10);
+console.log(isEqual);
+
+let isNotEqual = (10 != 5);
+console.log(isNotEqual);
+
+let isless = (10 < 5);
+console.log(isless);
+
+let isGreaterOrEqual = (10 >= 10);
+console.log(isGreaterOrEqual);
+
+let isLessOrEqual = (10 <= 10);
+console.log(isLessOrEqual);
